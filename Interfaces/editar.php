dca88bec6475de4d3568312b264a5ba59db590a4
AxelMontano <82304983+AxelMontano@users.noreply.github.com>
2023-10-23 01:31:22 -0700

    Add files via upload

--- Interfaces/editar.php
+++ Interfaces/editar.php
@@ -14,32 +14,30 @@
                             <div class="mb-3">
                                 <label for="proyecto">Seleccionar Proyecto:</label>
                                 <select name="proyecto" id="proyecto" class="form-control">
-                                <option value="" disabled selected>Seleccione uno:</option>
+                                    <option value="" disabled selected>Seleccione uno:</option>
                                     <?php
-                                    $servername = "localhost"; // Nombre del servidor MySQL
-                                    $username = "root";     // Nombre de usuario de la base de datos
-                                    $password = "";        // Contraseña de la base de datos
-                                    $database = "cfe"; // Nombre de la base de datos
+                                    $servername = "localhost";
+                                    $username = "root";
+                                    $password = "";
+                                    $database = "cfe";
 
-                                    // Crear una conexión
                                     $conexion = new mysqli($servername, $username, $password, $database);
 
-                                    // Verificar la conexión
                                     if ($conexion->connect_error) {
                                         die("Conexión fallida: " . $conexion->connect_error);
                                     }
 
-                                    // Consulta SQL para obtener los proyectos
-                                    $sql = "SELECT nomProyecto FROM proyecto";
+                                    $sql = "SELECT idProyecto, nomProyecto FROM proyecto";
                                     $result = $conexion->query($sql);
 
                                     if ($result->num_rows > 0) {
                                         while ($row = $result->fetch_assoc()) {
+                                            $idProyecto = $row['idProyecto'];
                                             $nombreProyecto = $row['nomProyecto'];
-                                            echo "<option value=\"$nombreProyecto\">$nombreProyecto</option>";
+                                            echo "<option value=\"$idProyecto\">$nombreProyecto</option>";
                                         }
                                     }
-                                    // Cierra la conexión a la base de datos
+
                                     $conexion->close();
                                     ?>
                                 </select>
@@ -59,17 +57,14 @@
 
 <script>
 function editarProyecto() {
-        var proyectoSelect = document.getElementById("proyecto");
-        var proyectoSeleccionado = proyectoSelect.value;
+    var proyectoSelect = document.getElementById("proyecto");
+    var proyectoSeleccionado = proyectoSelect.value;
 
-        if (!proyectoSeleccionado) {
-            alert("Por favor, seleccione un proyecto válido antes de editar.");
-        } else {
-            // Obtiene el índice seleccionado en el combo box
-            var selectedIndex = proyectoSelect.selectedIndex;
-            
-            // Redirige al usuario a la página de edición con el índice como parámetro de consulta
-            window.location.href = '../Interfaces/editar_proyecto.php?id=' + selectedIndex;
-        }
+    if (!proyectoSeleccionado) {
+        alert("Por favor, seleccione un proyecto válido antes de editar.");
+    } else {
+        // Redirige al usuario a la página de edición con la ID del proyecto seleccionado
+        window.location.href = '../Interfaces/editar_proyecto.php?id=' + proyectoSeleccionado;
     }
+}
 </script>
