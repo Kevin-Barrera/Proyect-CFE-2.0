76d329bbaf8444385300bba4c24edddf8fb5a0a6
LalokeraXD <lalokeraxxx@gmail.com>
2023-11-30 23:17:20 -0700

    Botón Guardar cambios

--- Interfaces/detalle_proyecto.php
+++ Interfaces/detalle_proyecto.php
@@ -32,12 +32,12 @@ require_once './header.php';
         echo "<p><strong>ID del Proyecto:</strong> " . $proyecto["idProyecto"] . "</p>";
         echo "<p><strong>Nombre del Proyecto:</strong> " . $proyecto["nomProyecto"] . "</p>";
         echo "<p><strong>Descripción:</strong> " . $proyecto["descProyecto"] . "</p>";
-        
+
 
         if ($tipo == 1) {
             echo "<div class='d-flex' style='justify-content: space-between;'>";
             echo "<p><strong>Detalles del Reporte Inicial</strong></p>";
-            echo "<button class='btn btn-success mr-4 mb-3' onclick='guardarCambios()'>Guardar Cambios</button>";
+            echo "<button class='btn btn-success mr-4 mb-3' onclick='guardarCambios($tipo)'>Guardar Cambios</button>";
             echo "</div>";
 
             // Obtener la ruta del archivo XLSX desde el campo idArchivo1
@@ -49,10 +49,13 @@ require_once './header.php';
             echo '<script src="../Js/mostrarTablaExcel.js"></script>';
             echo '<script>mostrarTablaExcel("' . $archivoXlsx . '");</script>';
         } elseif ($tipo == 2) {
-            echo "<p>Detalles del Reporte Final</p>";
+            echo "<div class='d-flex' style='justify-content: space-between;'>";
+            echo "<p><strong>Detalles del Reporte Inicial</strong></p>";
+            echo "<button class='btn btn-success mr-4 mb-3' onclick='guardarCambios($tipo)'>Guardar Cambios</button>";
+            echo "</div>";
             $archivoXlsx = "../Archivos/" . $proyecto["idArchivo2"] . ".xlsx";
             // echo '<p>' . $archivoXlsx . '</p>';
-            echo '<div id="tablaExcel"></div>';
+            echo '<div id="tablaExcel" style="max-width: 100%; overflow-x: auto; overflow-y: auto; height: 1000px;"></div>';
 
             // Llamar a la función desde el script JavaScript
             echo '<script src="../Js/mostrarTablaExcel.js"></script>';
@@ -65,9 +68,59 @@ require_once './header.php';
     $stmt_proyecto->close();
     $conexion->close();
     ?>
-    
-</div>
 
+</div>
+<script>
+    function guardarCambios($tipo) {
+        // Obtener la tabla actualizada
+        var table = document.getElementById('excelTable');
+
+        // Convertir la tabla a una hoja de cálculo
+        var updatedData = XLSX.utils.table_to_sheet(table);
+
+        // Crear un libro y agregar la hoja de cálculo actualizada
+        var wb = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, updatedData, "Sheet1");
+
+        // Obtener el nombre del archivo original del campo idArchivo del proyecto
+        if ($tipo == 1) {
+            var idArchivo = '<?php echo $proyecto["idArchivo1"]; ?>'; // Asegúrate de imprimir correctamente el valor en PHP
+        } else if ($tipo == 2) {
+            var idArchivo = '<?php echo $proyecto["idArchivo2"]; ?>';
+        }
+        console.log("ID Archivo:", idArchivo); // Agrega esta línea para verificar en la consola
+
+        // Convertir el libro a una cadena binaria
+        var wbout = XLSX.write(wb, {
+            bookType: 'xlsx',
+            bookSST: true,
+            type: 'binary'
+        });
+
+        // Convertir la cadena binaria a un Blob
+        var blob = new Blob([s2ab(wbout)], {
+            type: 'application/octet-stream'
+        });
+
+        // Crear un FormData para enviar el blob al servidor
+        var formData = new FormData();
+        formData.append('file', blob, idArchivo + '.xlsx');
+
+        // Enviar una solicitud al servidor
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'guardar_archivo.php', true);
+
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                alert("Cambios guardados con éxito en el archivo.");
+            } else {
+                alert("Error al guardar cambios en el archivo.");
+            }
+        };
+
+        xhr.send(formData);
+    }
+</script>
 <!-- Agrega este bloque de script al final de tu HTML -->
 <?php
 require_once './footer.php';
